a5709b17721e03c91d8a90a9e619e9cbe801d995
Kento Uchida <kento.apps@gmail.com>
2018-08-23 22:18:50 -0400

    Implement Form

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,8 +6,21 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
   String _email;
   String _password;
+
+  void _validateAndSave() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      print('Form is valid!');
+    } else {
+      print('Form is invalid');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +28,34 @@ class _LoginPageState extends State<LoginPage> {
         title: Text('Flutter login demo'),
       ),
       body: Container(
-        child: Text('Hello World'),
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                  validator: (value) =>
+                  value.isEmpty
+                      ? 'Email cannot be empty'
+                      : null,
+                  onSaved: (value) => _email = value,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  validator: (value) =>
+                  value.isEmpty
+                      ? 'Password cannot be empty'
+                      : null,
+                  onSaved: (value) => _password = value,
+                  obscureText: true,
+                ),
+                RaisedButton(
+                    child: Text('Login', style: TextStyle(fontSize: 20.0)),
+                    onPressed: _validateAndSave)
+              ],
+            )),
       ),
     );
   }
